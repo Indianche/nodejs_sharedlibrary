815d8a05455dead42f42bbbd2e22192c4a46d0de
Santhosh Durgam <durgamsanthosh141@gmail.com>
2020-09-21 16:40:41 +0530

    Update and rename deploy.groovy to deplloy.groovy

---
+++ vars/deplloy.groovy
@@ -0,0 +1,4 @@
+def call(Map stageParams) {
+ 
+    sh "sudo cp stageParams.fillepath /usr/share/tomcat/webapps"
+  }
--- vars/deploy.groovy
+++
@@ -1,4 +0,0 @@
-def call(Map stageParams) {
- 
-    sh "sudo cp stageParams.filepath /usr/share/tomcat/webapps"
-  }
